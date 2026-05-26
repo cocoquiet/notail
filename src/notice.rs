e73ca28e96be53f0e90a37f60e6900a24a877e20
JiSeong Lee <cocoquiet@knu.ac.kr>
2026-05-26 21:23:35 +0900

    Feat: Notice 가시성 적용

--- src/notice.rs
+++ src/notice.rs
@@ -1,15 +1,23 @@
-struct Notice {
+pub struct Notice {
     id: u32, // wr_id
     title: String,
     sca: u8, // 일반공지, 학사, 장학, 심컴, 첨컴, 인컴, 글솝, 플솝[구.심컴], ICT융합[학부], 대학원, 대학원 계약학과
     author: String,
     article: String,
     created_at: String,
-    updated_at: String
+    updated_at: String,
 }
 
 impl Notice {
-    fn new(id: u32, title: String, sca: u8, author: String, article: String, created_at: String, updated_at: String) -> Self {
+    pub fn new(
+        id: u32,
+        title: String,
+        sca: u8,
+        author: String,
+        article: String,
+        created_at: String,
+        updated_at: String,
+    ) -> Self {
         Notice {
             id,
             title,
@@ -17,7 +25,7 @@ impl Notice {
             author,
             article,
             created_at,
-            updated_at
+            updated_at,
         }
     }
 }
